b533a5a4cff94a0fb131fc16045844afbda0d1da
Sadman Saadat <sadmansaadat99@gmail.com>
2021-08-06 00:52:46 +0600

    Implement Payment Math Olympiad

--- routes/MathOlympiad.routes.js
+++ routes/MathOlympiad.routes.js
@@ -10,12 +10,14 @@ const {
     getMO,
     postMO,
     getMOList,
-    deleteMO
+    deleteMO,
+    paymentDoneMO
 } = require("../controllers/mathOlympiad.controller")
 
 router.get('/register', ensureAuthenticated, addUserData, getMO);
 router.post('/register', ensureAuthenticated, addUserData, postMO);
 router.get('/list', ensureAuthenticated, addUserData, getMOList);
 router.get('/delete/:id', ensureAuthenticated, addUserData, deleteMO);
+router.get('/paymentDone/:id', ensureAuthenticated, addUserData, paymentDoneMO);
 
 module.exports = router;
